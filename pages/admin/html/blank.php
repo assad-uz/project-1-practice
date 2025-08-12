a8ee7ad420d3e6c95e4d4cc8c53d7e24006372f9
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-08-12 13:36:00 +0600

    Update

---
+++ pages/admin/html/blank.php
@@ -0,0 +1,15 @@
+      <?php 
+      require_once("../includes/aheader.php")
+      ?>
+
+      <?php 
+      require_once("../includes/anavbar.php")
+      ?>
+
+      <?php 
+      require_once("../includes/asidebar.php")
+      ?>
+
+      <?php 
+      require_once("../includes/afooter.php")
+      ?>
